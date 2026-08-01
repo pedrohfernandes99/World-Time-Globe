e97a736e8695b0595d2dd223a5dda5c8230ed018
Pedro <pedrohfernandes99@hotmail.com>
2026-08-01 09:08:50 -0700

    Add project configuration module

---
+++ js/core/Config.js
@@ -0,0 +1,22 @@
+const Config = {
+
+    globe: {
+
+        autoRotate: true,
+        autoRotateSpeed: 0.4,
+
+        backgroundColor: "#000814",
+
+        atmosphereColor: "#3fa9ff",
+
+        atmosphereAltitude: 0.15
+
+    },
+
+    camera: {
+
+        distance: 250
+
+    }
+
+};
