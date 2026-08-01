ca1e5973854f4b0868ffbc4bdd49aa434d760928
Pedro <pedrohfernandes99@hotmail.com>
2026-08-01 13:14:07 -0700

    Support MultiPolygon countries

--- js/world/Countries.js
+++ js/world/Countries.js
@@ -2,6 +2,50 @@ const Countries = {
 
     group: null,
 
+    radius: 1.002,
+
+    latLngToVector3(lat, lng, radius) {
+
+        const phi = (90 - lat) * (Math.PI / 180);
+        const theta = (lng + 180) * (Math.PI / 180);
+
+        return new THREE.Vector3(
+            -(radius * Math.sin(phi) * Math.cos(theta)),
+            radius * Math.cos(phi),
+            radius * Math.sin(phi) * Math.sin(theta)
+        );
+
+    },
+
+    drawLine(coordinates) {
+
+        const points = [];
+
+        coordinates.forEach(coord => {
+
+            const lng = coord[0];
+            const lat = coord[1];
+
+            points.push(
+                this.latLngToVector3(lat, lng, this.radius)
+            );
+
+        });
+
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+        const material = new THREE.LineBasicMaterial({
+
+            color: 0x00ff99
+
+        });
+
+        const line = new THREE.Line(geometry, material);
+
+        this.group.add(line);
+
+    },
+
     init(scene) {
 
         this.group = new THREE.Group();
@@ -15,21 +59,54 @@ const Countries = {
     createCountries() {
 
         if (!CountryLoader.data) {
-
+    
             console.error("GeoJSON não carregado.");
-
+    
             return;
-
+    
         }
-
-        console.log("Criando países...");
-
+    
         CountryLoader.data.features.forEach(country => {
-
-            console.log(country.properties);
-
+    
+            const geometry = country.geometry;
+    
+            switch (geometry.type) {
+    
+                case "Polygon":
+    
+                    geometry.coordinates.forEach(ring => {
+    
+                        this.drawLine(ring);
+    
+                    });
+    
+                    break;
+    
+                case "MultiPolygon":
+    
+                    geometry.coordinates.forEach(polygon => {
+    
+                        polygon.forEach(ring => {
+    
+                            this.drawLine(ring);
+    
+                        });
+    
+                    });
+    
+                    break;
+    
+                default:
+    
+                    console.warn(
+                        "Tipo não suportado:",
+                        geometry.type
+                    );
+    
+            }
+    
         });
-
+    
     },
 
     update() {
